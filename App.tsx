6097fc245d83305e7070a342c211ea162d2906e6
Janne <bhi256@myy.haaga-helia.fi>
2025-04-08 17:02:19 +0300

    Added react-native-paper and fixed module resolution for typescript

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { PaperProvider} from 'react-native-paper';
 
 import HomeScreen from './screens/homeScreen';
 import NewStory from './screens/newStory';
@@ -10,11 +11,13 @@ const Stack = createStackNavigator<NavigatorParams>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <Stack.Navigator id={undefined}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="NewStory" component={NewStory} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator id={undefined}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="NewStory" component={NewStory} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -1,18 +1,20 @@
 import { Button, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from '@react-navigation/stack';
-
+import { Appbar } from "react-native-paper";
 import { NavigatorParams } from "../resources/customTypes";
 import styles from "../resources/styles"
 
 type homeScreenProp = StackNavigationProp<NavigatorParams>;
 
-export default function HomeScreen () {
+export default function HomeScreen() {
     const navigation = useNavigation<homeScreenProp>();
 
     return (
         <View style={styles.center}>
-            <Text>HomeScreen</Text>
+            <Appbar mode="medium" elevated>
+                <Appbar.Content title="HomeScreen" />
+            </Appbar>
             <Text>Will be implemented shortly!</Text>
             <Button title="Add Story" onPress={() => navigation.navigate('NewStory')} />
         </View>
