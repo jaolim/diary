f4c1e1c864c5bf6ca356e89733e2ba23a53867af
Janne <bhi256@myy.haaga-helia.fi>
2025-04-08 15:16:25 +0300

    Added files for styles and types and first 2 screens

---
+++ screens/homeScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import styles from "../resources/styles"
+
+export default function HomeScreen () {
+
+    return (
+        <View style={styles.center}>
+            <Text>HomeScreen</Text>
+            <Text>Will be implemented shortly!</Text>
+        </View>
+    )
+}
---
+++ screens/newStory.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import styles from "../resources/styles"
+
+export default function NewStory() {
+
+    return (
+        <View style={styles.center}>
+            <Text>New Story</Text>
+            <Text>Will be implemented shortly!</Text>
+        </View>
+    )
+}
